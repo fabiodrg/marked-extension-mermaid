033b502c175e2d0bbee2b77896236347b9adf451
Fabio Gaspar <fabiodanielreis@hotmail.com>
2022-04-15 21:15:17 +0100

    wip: Experiment integrating 'mermaid' to generate the SVG diagrams
    - Adds `puppeteer` to run a headless browser. It allows to run the
      `mermaid` in a browser context and render the SVG
    - The issue is that these APIs are async and `marked` still does not
      support async parsers, altough some WIP exists: https://github.com/markedjs/marked/pull/2405

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+import puppeteer from 'puppeteer';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function renderSvg(code) {
+  const browser = await puppeteer.launch({
+    // headless: false // uncomment for debugging purposes
+  });
+  const page = await browser.newPage();
+  await page.goto(`file://${path.join(__dirname, 'render.html')}`);
+  const svg = await page.evaluate((html) => {
+    // update DOM with mermaid diagram code
+    const $container = document.querySelector('#container');
+    $container.innerHTML = html;
+    // run mermaid
+    window.mermaid.init();
+    // return the SVG
+    return document.querySelector('#container svg').outerHTML;
+  }, toHtml(code));
+  return svg;
+}
+
+(async() => {
+const svg = await renderSvg('graph TD;A-->B;A-->C;B-->D;C-->D;');
+console.log(svg);
+})();
+
 function toHtml(code) {
   return '<div class="mermaid">\n' + code + '\n</div>\n';
 }
